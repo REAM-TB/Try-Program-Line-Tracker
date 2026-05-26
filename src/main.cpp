98d6b7a81df3db4cf2b87b52a1fa44e7889279b3
ColdKYZ <5022231153@student.its.ac.id>
2026-05-26 23:23:04 +0700

    5/26 11.22 Delay 1 Detik di awal

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include "bacaSensor.h"
 #include "button.h"
+#include "buzzer.h"
 #include "PIDController.h"
 #include "motorControl.h"
+#include "bacaSensor.h"
 
 int base_speed = 160;
 int maxSpeed = 180;
@@ -12,10 +14,12 @@ float Kd = 0.8;
 
 
 bool pidEnabled = false;
+bool startupDone = false;
 
 
 void setup() {
   Serial.begin(115200);
+  inisialisasibuzzer();  
   initBacaSensor();
   initMotorControl();
   initButton();
@@ -29,15 +33,24 @@ void loop() {
     pidEnabled = updatedPidEnabled;
 
     if (pidEnabled) {
+      startupDone = false;
       resetPIDState();
       Serial.println("PID ON");
     } else {
       motorStop();
+      startupDone = false;
       Serial.println("PID OFF");
     }
   }
 
   if (pidEnabled) {
+    if (!startupDone) {
+      motorForward(base_speed, base_speed);
+      delay(1000);
+      motorStop();
+      startupDone = true;
+      resetPIDState();
+    }
     PID(base_speed, Kp, Kd);
   } else {
     motorStop();
